a358a653762b001a0732c67105f50d19233fcd4c
syaidart123 <syaidaja23@gmail.com>
2026-03-25 15:56:08 +0700

    fix: header search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,67 +17,6 @@ export default function ArticlePage() {
       <HeroSection />
       <TabSelector />
 
-      <section className="hero py-8 relative">
-        <div className="container z-20 ">
-          <div className="text-center">
-            <h2
-              className="text-center color-#ffffff text-black lg:font-size-4xl"
-              data-translation="true"
-              data-us="Mekari user guide"
-              data-id="Panduan pengguna Mekari"
-            >
-              Panduan pengguna Mekari
-            </h2>
-          </div>
-          <div className="search rounded max-w-md my-4 mx-auto">
-            <h2 className="sr-only">Pencarian</h2>
-
-            <form
-              role="search"
-              className="form-field py-2 border rounded bg-white mb-0 transition"
-              data-search=""
-              data-instant="true"
-              action="/hc/id/search"
-              accept-charset="UTF-8"
-              method="get"
-            >
-              <input type="hidden" name="utf8" value="✓" />
-              <input
-                type="search"
-                name="query"
-                id="query"
-                placeholder="Temukan panduan di sini..."
-                aria-label="Temukan panduan di sini..."
-              />
-            </form>
-
-            <svg
-              className="search-icon transition"
-              xmlns="http://www.w3.org/2000/svg"
-              focusable="false"
-              viewBox="0 0 12 12"
-              aria-hidden="true"
-            >
-              <circle
-                cx="4.5"
-                cy="4.5"
-                r="4"
-                fill="none"
-                stroke="currentColor"
-              ></circle>
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                d="M11 11L7.5 7.5"
-              ></path>
-            </svg>
-            <div className="absolute z-10 top-0 left-0 h-full w-full bg-cover bg-center"></div>
-
-            <div className="absolute z-0 top-0 left-0 h-full w-full"></div>
-          </div>
-        </div>
-      </section>
-
       {/* Section Title */}
       <div className="bg-white py-10 border-b border-slate-100">
         <div className="max-w-340 mx-auto px-5 sm:px-8 text-center">
